c329339164beca850c6c7e663d121f55d1fd5975
Marielly <mariellysales01@gmail.com>
2024-09-23 22:33:41 -0300

    Base do projeto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, mundo!</h1>
+      <p>Meu primeiro app</p>
+    </div>
+  );
+}
+
+export default App;
