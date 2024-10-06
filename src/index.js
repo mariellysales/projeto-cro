451a61c91f082f46d8d378f008d7f49316e3f9e9
Marielly <mariellysales01@gmail.com>
2024-10-05 21:34:11 -0300

    implementação header
    responsividade mobile
    
    funcinalidades links de compartilhamento para  redes sociais
    
    implementação de logo
    
    padronização de projeto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './Header';
+import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
