d35286302fce1a8695d7ed4d5405340c520c4745
Marielly Sales <mariellysales01@icloud.com>
2024-10-29 22:16:20 -0300

    ajustes botões, ajustes informações dos fatos, inclusão url

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,43 +1,23 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Contact.module.css';
-import ReactCardFlip from 'react-card-flip';
 import { MdOutlineEmail, MdEmail } from "react-icons/md";
 import { BsClipboard, BsClipboardCheck } from "react-icons/bs";
 import { FaUserInjured, FaUserCheck } from "react-icons/fa";
 
 function Contact() {
-  const [isFlipped, setIsFlipped] = useState({
-    email: false,
-    exam: false,
-    service: false,
-  });
-
-  function handleMouseEnter(button) {
-    setIsFlipped((prev) => ({ ...prev, [button]: true }));
-  }
-
-  function handleMouseLeave(button) {
-    setIsFlipped((prev) => ({ ...prev, [button]: false }));
-  }
-
   return (
     <div className={styles.contactSection}>
       <div className={styles.buttons}>
         <div className={styles.buttonContainer}>
           <a href="https://www.cropr.org.br/index.php/contato" target="_blank" rel="noopener noreferrer">
-            <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped.email}>
-              <div
-                className={styles.awarenessButton}
-                onMouseOver={() => handleMouseEnter('email')}
-              >
-                <div className={styles.front}>
-                  <MdOutlineEmail className={styles.frontIcon} />
-                </div>
+            <div className={styles.awarenessButton}>
+              <div className={styles.front}>
+                <MdOutlineEmail className={styles.frontIcon} />
               </div>
-              <div className={`${styles.awarenessButton} ${styles.back}`} onMouseLeave={() => handleMouseLeave('email')}>
+              <div className={styles.back}>
                 <MdEmail className={styles.backIcon} />
               </div>
-            </ReactCardFlip>
+            </div>
           </a>
           <div className={styles.icon}>
             <p>Fale conosco</p>
@@ -45,38 +25,28 @@ function Contact() {
         </div>
 
         <div className={styles.buttonContainer}>
-          <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped.exam}>
-            <div
-              className={styles.awarenessButton}
-              onMouseOver={() => handleMouseEnter('exam')}
-            >
-              <div className={styles.front}>
-                <BsClipboard className={styles.frontIcon} />
-              </div>
+          <div className={styles.awarenessButton}>
+            <div className={styles.front}>
+              <BsClipboard className={styles.frontIcon} />
             </div>
-            <div className={`${styles.awarenessButton} ${styles.back}`} onMouseLeave={() => handleMouseLeave('exam')}>
+            <div className={styles.back}>
               <BsClipboardCheck className={styles.backIcon} />
             </div>
-          </ReactCardFlip>
+          </div>
           <div className={styles.icon}>
             <p>Autoexame</p>
           </div>
         </div>
 
         <div className={styles.buttonContainer}>
-          <ReactCardFlip flipDirection="horizontal" isFlipped={isFlipped.service}>
-            <div
-              className={styles.awarenessButton}
-              onMouseOver={() => handleMouseEnter('service')}
-            >
-              <div className={styles.front}>
-                <FaUserInjured className={styles.frontIcon} />
-              </div>
+          <div className={styles.awarenessButton}>
+            <div className={styles.front}>
+              <FaUserInjured className={styles.frontIcon} />
             </div>
-            <div className={`${styles.awarenessButton} ${styles.back}`} onMouseLeave={() => handleMouseLeave('service')}>
+            <div className={styles.back}>
               <FaUserCheck className={styles.backIcon} />
             </div>
-          </ReactCardFlip>
+          </div>
           <div className={styles.icon}>
             <p>Atendimento</p>
           </div>
--- src/components/Facts.js
+++ src/components/Facts.js
@@ -11,13 +11,13 @@ const factsData = [
         id: 2,
         imgSrc: "/images/brazil.png",
         alt: "Gráfico mostrando casos de câncer bucal no Brasil",
-        text: "Aproximadamente 15.190 novos casos de câncer bucal são diagnosticados a cada ano no Brasil"
+        text: "Aproximadamente 15.100 casos de câncer bucal no Brasil nos últimos 3 anos"
     },
     {
         id: 3,
         imgSrc: "/images/tape.png",
         alt: "Gráfico mostrando mortes por câncer bucal mundialmente",
-        text: "O câncer bucal é responsável por cerca de 177.000 mortes por ano mundialmente"
+        text: "O câncer bucal foi responsável por cerca de 6.192 mortes em 2020 no Brasil"
     }
 ];
 
@@ -27,7 +27,7 @@ function Facts() {
             <h1 className={styles.factsTitle}>Fatos sobre o câncer bucal</h1>
 
             <div className={styles.factsWrapper}>
-                {factsData.map(({id, imgSrc, alt, text }) => (
+                {factsData.map(({ id, imgSrc, alt, text }) => (
                     <div className={styles.factBox} key={id}>
                         <div className={styles.factImageContainer}>
                             <img src={imgSrc} alt={alt} className={styles.factImage} />
